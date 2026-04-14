8da6a9362693a470eefd282dc2ebbda051d7bf9d
JoshuaMarinoo <JoshuaMarino108@gmail.com>
2026-04-14 15:27:29 -0400

    Initial Commit

---
+++ RedefineLighting/RedefineLightingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedefineLightingApp.swift
+//  RedefineLighting
+//
+//  Created by Joshua Marino on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct RedefineLightingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
