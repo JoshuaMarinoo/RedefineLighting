2c54faeb1079f68b877b60ecce890c81c820071e
JoshuaMarinoo <JoshuaMarino108@gmail.com>
2026-04-15 01:11:34 -0400

    App Camera Access
    Allowing the app to access the camera and display it on the UI

---
+++ RedefineLighting/CameraPreviewView.swift
@@ -0,0 +1,34 @@
+//
+//  CameraPreviewView.swift
+//  RedefineLighting
+//
+//  Created by Joshua Marino on 4/14/26.
+//
+import SwiftUI
+import AVFoundation
+import UIKit
+
+final class PreviewUIView: UIView {
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+
+    var previewLayer: AVCaptureVideoPreviewLayer {
+        layer as! AVCaptureVideoPreviewLayer
+    }
+}
+
+struct CameraPreviewView: UIViewRepresentable {
+    let session: AVCaptureSession
+
+    func makeUIView(context: Context) -> PreviewUIView {
+        let view = PreviewUIView()
+        view.previewLayer.session = session
+        view.previewLayer.videoGravity = .resizeAspectFill
+        return view
+    }
+
+    func updateUIView(_ uiView: PreviewUIView, context: Context) {
+        uiView.previewLayer.session = session
+    }
+}
